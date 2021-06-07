659e7bbd93dbda118c8cbadd17f31d8c4100d474
Madelene Campos <madelenecampos@gmail.com>
2021-06-07 15:00:38 -0400

    Creates user authentication - registration system

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '38723e6a814929ae54a24afe7815064d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
